ec7403230d5ca22548ed0f50e6c4289a8c81fa95
DESKTOP-08G0K7I\DuongSB <duongnk.techkids@gmail.com>
2018-07-09 10:13:04 +0700

    Trying to Deploy

--- server.js
+++ server.js
@@ -10,12 +10,24 @@ app.use(cors())
 
 app.get('/questions', (req, res) => {
   res.send(
-    [{
-      title: "Hello World!",
-      description: "Hi there! How are you?"
-    }]
+    [
+      {
+        title: "Hello World!",
+        description: "Hi there! How are you?"
+      }
+    ]
   )
 })
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 8081;
 
