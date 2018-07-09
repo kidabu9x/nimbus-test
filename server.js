2e381c591b3f4af513463f71957b48d51b2575e6
DESKTOP-08G0K7I\DuongSB <duongnk.techkids@gmail.com>
2018-07-09 10:56:30 +0700

    Try to deploy again

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.get('/questions', (req, res) => {
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('dist'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
   });
 }
 
