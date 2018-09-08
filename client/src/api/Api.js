f90695934fb04d14ffc2c916425756af6de0b393
DESKTOP-08G0K7I\DuongSB <duongnk.techkids@gmail.com>
2018-09-08 15:08:41 +0700

    trying to fix css buggy

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:6969`
-    // baseURL: ''
+    // baseURL: `http://localhost:6969`
+    baseURL: ''
   })
 }
