16acfdf0b1a2d07385a6ab172e7828b69be5a37d
kidabu9x <duongnk.techkids@gmail.com>
2018-10-08 04:33:30 +0700

    deploy new version to heroku

--- server.js
+++ server.js
@@ -47,20 +47,20 @@ const lession = require('./routes/api/courseApi/lessionApi')
 app.use('/api/lession', lession)
 
 // Uncomment when deploy to server
-app.use(express.static('dist'));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-});
+// app.use(express.static('dist'));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+// });
 
 // Serve static assets if in production
-// if (process.env.NODE_ENV === 'production') {
-//   // Set static folder
-//   app.use(express.static('dist'));
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('dist'));
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-//   });
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 8080;
 // const port = process.env.PORT || 6969;
