9e24d7a5bf041d104793dd02ebe45ea4530b3625
Nguyen Khanh Duong <v.duongnk@vinid.net>
2020-12-16 14:17:14 +0700

    fix connect mongo

--- client/src/api/ApiConfig.js
+++ client/src/api/ApiConfig.js
@@ -1,6 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production';
 const BASE_URL = isProd ? 'https://nimbus.edu.vn/api' : 'http://localhost:5000/api';
-// const BASE_URL = '/api';
 
 module.exports = {
   BASE_URL: BASE_URL
